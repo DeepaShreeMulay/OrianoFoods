57ae2de717046ad5d52b5875fc71df273398d6df
Deepashree Mulay <deepashree139@gmail.com>
2020-11-29 14:03:46 +0530

    1. Slash Activity final commit

--- app/src/orderOnline/java/com/atlas/orianofood/activity/SplashActivity.kt
+++ app/src/orderOnline/java/com/atlas/orianofood/activity/SplashActivity.kt
@@ -2,10 +2,13 @@ package com.atlas.orianofood.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Window
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.atlas.orianofood.R
-import kotlinx.android.synthetic.orderOnline.activity_splash_screen.*
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +16,21 @@ class SplashActivity : AppCompatActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_splash_screen)
 
-        btn_start_order.setOnClickListener {
+
+        val animSlideUp = AnimationUtils.loadAnimation(
+            this,
+            R.anim.slide_up
+        )
+        // Slide Up
+
+        iv_oriano.startAnimation(animSlideUp)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            /* Create an Intent that will start the Menu-Activity. */
             val loginIntent = Intent(this, LoginActivity::class.java)
             startActivity(loginIntent)
             finish()
-        }
+        }, 1500)
+
     }
 }
